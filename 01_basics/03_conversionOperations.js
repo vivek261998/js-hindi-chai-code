82eb7ce0121bb1a294b0fe844dee3acf5073b477
vivek261998 <109942751+vivek261998@users.noreply.github.com>
2026-04-03 06:22:13 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score = "vivek"
+
+// console.log(typeof score)
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0 
+
+// let isLoggedIn = "vi" 
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//"" => False
+//1=>True; 2=>False
+//"Vivek" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
